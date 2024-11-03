e29f6cd4a35cc4c745e377bef55905eb6e0b53b4
Oswaldo Palacios Perez <yaco2002@live.com.mx>
2024-11-02 18:45:44 -0600

    add function post

--- client/src/views/Products.tsx
+++ client/src/views/Products.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
 
 export default function Products() {
   return (
-    <div>
-      <p>products</p>
-    </div>
+    <>
+      <div className="flex justify-between items-center">
+        <h2 className="text-4xl font-black text-slate-500">Productos</h2>
+        <Link to="products/new" className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500">Agregar Producto</Link>
+      </div>
+    </>
   )
 }

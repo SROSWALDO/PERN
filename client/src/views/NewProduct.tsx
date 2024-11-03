67c2bca9df5534b144346c028efe73c96d7ced68
Oswaldo Palacios Perez <yaco2002@live.com.mx>
2024-11-02 19:00:56 -0600

    add post product

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,4 +1,4 @@
-import { ActionFunctionArgs, Form, Link, useActionData } from "react-router-dom";
+import { ActionFunctionArgs, Form, Link, redirect, useActionData } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
 import { addProduct } from "../services/ProductServoce";
 
@@ -13,9 +13,9 @@ export async function action({request} : ActionFunctionArgs) {
     return error
   }
 
-  addProduct(data)
+  await addProduct(data)
 
-  return {}
+  return redirect('/')
 
 }
 
